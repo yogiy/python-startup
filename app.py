542c0ac7557e95b7beb0719c142799b1a7505512
Yogesh <yogesh@nestorbird.com>
2024-03-18 16:50:53 +0530

    code

--- app.py
+++ app.py
@@ -1,9 +1,54 @@
 from flask import Flask
+import requests
+import json
+from bs4 import BeautifulSoup
+
 app = Flask(__name__)
+post_URL = "http://www.livedinn.com/jokes_server/updatehoroscope.php"
+
+signs = [
+    "aries",
+    "libra",
+    "taurus",
+    "gemini",
+    "cancer",
+    "leo",
+    "virgo",
+    "scorpio",
+    "sagittarius",
+    "capricon",
+    "aquarius",
+    "pisces"
+]
+
 
 @app.route("/")
 def hello():
-    return "Hello Yogi!"
+    for sign in signs:
+        # get_URL = f"https://www.washingtonpost.com/entertainment/horoscopes/{sign}/"
+        get_URL = f"https://www.astrotak.com/daily-horoscope/{sign}-daily-horoscope"
+
+        headers = {'Content-Type': 'application/json', 'User-Agent': 'Mozilla/5.0'}
+        page = requests.get(get_URL)
+        soup = BeautifulSoup(page.content, "html.parser")
+        results = soup.find("script", {"id": "__NEXT_DATA__"})
+        
+        
+        predition = results.text.strip()
+        res = json.loads(predition)
+        result = res['props']['pageProps']['zodiacData']['today']['prediction']
+        body = {'sign': sign, 'txt' : json.dumps(result) }
+        
+
+        headers = {'Content-Type': 'application/json', 'User-Agent': 'PostmanRuntime/7.36.3'}
+        session = requests.Session()
+        session.headers.update(headers)
+
+        response = session.post(post_URL, data=json.dumps(body))
+        # print(f"{sign}: {response.__dict__ }")
+        print(response.status_code)
+        print(response.text)
+
 
 if __name__ == "__main__":
     app.run()
